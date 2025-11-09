cd99d8362a91a0f05bd8b8d98803f2c4e10523ae
taro-furuya <t.furuya1005@gmail.com>
2025-11-09 12:21:38 +0900

    Create Corporate component with contact form

---
+++ src/views/Corporate.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../components/Card";
+
+export default function Corporate() {
+  return (
+    <Card title="法人向けお問い合わせ（ダミー）">
+      <form className="grid md:grid-cols-2 gap-2">
+        <input className="border rounded px-3 py-2" placeholder="会社名" />
+        <input className="border rounded px-3 py-2" placeholder="ご担当者名" />
+        <input className="border rounded px-3 py-2 md:col-span-2" placeholder="メールアドレス" />
+        <textarea className="border rounded px-3 py-2 md:col-span-2 h-24" placeholder="お問い合わせ内容" />
+        <div className="md:col-span-2 flex justify-end">
+          <button type="button" className="px-4 py-2 rounded-xl bg-black text-white">
+            送信（ダミー）
+          </button>
+        </div>
+      </form>
+    </Card>
+  );
+}
