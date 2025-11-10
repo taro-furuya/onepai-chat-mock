aa33bf39b54288a6bd2f2c6b287928710dd022f4
taro-furuya <t.furuya1005@gmail.com>
2025-11-10 10:59:48 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-// src/App.tsx
-import React from "react";
+import React, { useEffect, useSyncExternalStore } from "react";
+import Nav from "./components/Nav";
 import Shop from "./views/Shop";
 import Guidelines from "./views/Guidelines";
 import Corporate from "./views/Corporate";
 
+const useHash = () =>
+  useSyncExternalStore(
+    (cb) => { window.addEventListener("hashchange", cb); return () => window.removeEventListener("hashchange", cb); },
+    () => window.location.hash || "#/",
+    () => "#/"
+  );
+
 const App: React.FC = () => {
-  const hash = typeof window !== "undefined" ? window.location.hash : "";
+  const hash = useHash();
 
-  const gotoCorporate = () => {
-    window.location.hash = "#/corporate";
-  };
+  const gotoCorporate = () => { window.location.hash = "#/corporate"; };
 
-  const gotoGuidelines = () => {
-    window.location.hash = "#/guidelines";
-  };
+  let View: React.ReactNode = <Shop gotoCorporate={gotoCorporate} />;
+  if (hash.startsWith("#/guidelines")) View = <Guidelines />;
+  if (hash.startsWith("#/corporate")) View = <Corporate />;
 
-  // 簡易ルーティング（ハッシュ）
-  if (hash.startsWith("#/guidelines")) return <Guidelines />;
-  if (hash.startsWith("#/corporate")) return <Corporate />;
+  useEffect(() => {
+    if (!window.location.hash) window.location.hash = "#/";
+  }, []);
 
-  // ★ Shop へ必須の gotoCorporate を渡す
-  return <Shop gotoCorporate={gotoCorporate} />;
+  return (
+    <>
+      <Nav />
+      {View}
+    </>
+  );
 };
 
 export default App;
